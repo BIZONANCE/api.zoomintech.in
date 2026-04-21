a21f320237b08fa6cbf428857660a11f6bf2c925
bizonance <bizonance@gmail.com>
2026-04-21 13:17:37 +0530

    delete

--- src/routes/serviceDetail.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { getServiceDetail } from "../controllers/serviceDetail.controller";
-
-const router = Router();
-
-router.get("/:slug", getServiceDetail);
-
-export default router;
